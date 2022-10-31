bba32cfe5478f94695989da9f23f22576525c98d
Magali Morin <magali@lukso.io>
2022-10-31 14:45:08 +0100

    Modify _app.js file

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // return <Component {...pageProps} />
+  return (
+    <div>
+      <p>Je suis une page static</p>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
